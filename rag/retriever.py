e989b456bdce0c22260ebd648dfd73ea3ba4ffa5
IoannisKotsis <john01ko18@gmail.com>
2026-07-08 21:19:24 +0300

    Add retriever tool

---
+++ rag/retriever.py
@@ -0,0 +1,27 @@
+# Import necessary modules
+import chromadb
+
+# Open a new Chromadb connection
+client = chromadb.PersistentClient("../chroma_db")
+collection = client.get_or_create_collection("diabetes_guidelines")
+
+# Return relative chunks
+
+def retrieve(question, n_results=3):
+    relative_chunks = collection.query(query_texts=[question], n_results=n_results)
+    
+    documents_list = relative_chunks['documents'][0]
+    metadatas_list = relative_chunks['metadatas'][0]
+    
+    results = []
+    for doc, meta in zip(documents_list, metadatas_list):
+        doc_meta = {'text': doc, 'source': meta['source']}
+        results.append(doc_meta)
+    
+    return results
+        
+
+for r in retrieve("I feel shaky and sweaty"):
+    print(r["source"])
+    print(r["text"][:80])
+    print("---")
